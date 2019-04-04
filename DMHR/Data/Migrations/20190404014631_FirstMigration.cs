00171fda6a6942cec9a308d34aba9dfce14db45b
InZeyn <43585311+InZeyn@users.noreply.github.com>
2019-04-03 21:58:51 -0400

    Models added

---
+++ DMHR/Data/Migrations/20190404014631_FirstMigration.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DMHR.Data.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cargos",
+                columns: table => new
+                {
+                    CargoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cargos", x => x.CargoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departamentos",
+                columns: table => new
+                {
+                    DepartamentoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CodigoDepartamento = table.Column<int>(nullable: false),
+                    Nombre = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos", x => x.DepartamentoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nominas",
+                columns: table => new
+                {
+                    NominaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Year = table.Column<DateTime>(nullable: false),
+                    Month = table.Column<DateTime>(nullable: false),
+                    MontoTotal = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nominas", x => x.NominaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    EmpleadoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CodigoEmpleado = table.Column<int>(nullable: false),
+                    Nombre = table.Column<string>(nullable: false),
+                    Apellido = table.Column<string>(nullable: false),
+                    Telefono = table.Column<string>(nullable: true),
+                    FechaIngreso = table.Column<DateTime>(nullable: false),
+                    Salario = table.Column<int>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    CargoId = table.Column<int>(nullable: false),
+                    DepartamentoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.EmpleadoId);
+                    table.ForeignKey(
+                        name: "FK_Empleados_Cargos_CargoId",
+                        column: x => x.CargoId,
+                        principalTable: "Cargos",
+                        principalColumn: "CargoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Empleados_Departamentos_DepartamentoId",
+                        column: x => x.DepartamentoId,
+                        principalTable: "Departamentos",
+                        principalColumn: "DepartamentoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GetLicencias",
+                columns: table => new
+                {
+                    LicenciasId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Desde = table.Column<DateTime>(nullable: false),
+                    Hasta = table.Column<DateTime>(nullable: false),
+                    Motivo = table.Column<string>(nullable: true),
+                    Comentarios = table.Column<string>(nullable: true),
+                    IsPaid = table.Column<bool>(nullable: false),
+                    EmpleadoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GetLicencias", x => x.LicenciasId);
+                    table.ForeignKey(
+                        name: "FK_GetLicencias_Empleados_EmpleadoId",
+                        column: x => x.EmpleadoId,
+                        principalTable: "Empleados",
+                        principalColumn: "EmpleadoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GetPermisos",
+                columns: table => new
+                {
+                    PermisosId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Desde = table.Column<DateTime>(nullable: false),
+                    Hasta = table.Column<DateTime>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Comentarios = table.Column<string>(nullable: true),
+                    IsPaid = table.Column<bool>(nullable: false),
+                    EmpleadoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GetPermisos", x => x.PermisosId);
+                    table.ForeignKey(
+                        name: "FK_GetPermisos_Empleados_EmpleadoId",
+                        column: x => x.EmpleadoId,
+                        principalTable: "Empleados",
+                        principalColumn: "EmpleadoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GetVacaciones",
+                columns: table => new
+                {
+                    VacacionesId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Desde = table.Column<DateTime>(nullable: false),
+                    Hasta = table.Column<DateTime>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Comentarios = table.Column<string>(nullable: true),
+                    IsPaid = table.Column<bool>(nullable: false),
+                    EmpleadoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GetVacaciones", x => x.VacacionesId);
+                    table.ForeignKey(
+                        name: "FK_GetVacaciones_Empleados_EmpleadoId",
+                        column: x => x.EmpleadoId,
+                        principalTable: "Empleados",
+                        principalColumn: "EmpleadoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalidadeEmpleados",
+                columns: table => new
+                {
+                    SalidaEmpleadoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TipoDeSalida = table.Column<int>(nullable: false),
+                    Motivo = table.Column<string>(nullable: true),
+                    FechaSalida = table.Column<DateTime>(nullable: false),
+                    EmpleadoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalidadeEmpleados", x => x.SalidaEmpleadoId);
+                    table.ForeignKey(
+                        name: "FK_SalidadeEmpleados_Empleados_EmpleadoId",
+                        column: x => x.EmpleadoId,
+                        principalTable: "Empleados",
+                        principalColumn: "EmpleadoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empleados_CargoId",
+                table: "Empleados",
+                column: "CargoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empleados_DepartamentoId",
+                table: "Empleados",
+                column: "DepartamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GetLicencias_EmpleadoId",
+                table: "GetLicencias",
+                column: "EmpleadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GetPermisos_EmpleadoId",
+                table: "GetPermisos",
+                column: "EmpleadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GetVacaciones_EmpleadoId",
+                table: "GetVacaciones",
+                column: "EmpleadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalidadeEmpleados_EmpleadoId",
+                table: "SalidadeEmpleados",
+                column: "EmpleadoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GetLicencias");
+
+            migrationBuilder.DropTable(
+                name: "GetPermisos");
+
+            migrationBuilder.DropTable(
+                name: "GetVacaciones");
+
+            migrationBuilder.DropTable(
+                name: "Nominas");
+
+            migrationBuilder.DropTable(
+                name: "SalidadeEmpleados");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+
+            migrationBuilder.DropTable(
+                name: "Cargos");
+
+            migrationBuilder.DropTable(
+                name: "Departamentos");
+        }
+    }
+}
--- DMHR/Models/AppModels.cs
+++ DMHR/Models/AppModels.cs
@@ -10,32 +10,42 @@ namespace DMHR.Models
     public class Empleado
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int EmpleadoId { get; set; }
-        
+        [Required]
         public int CodigoEmpleado { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public string Apellido { get; set; }
+        [Phone]
         public string Telefono { get; set; }
         public DateTime FechaIngreso { get; set; }
+        [Required]
         public int Salario { get; set; }
         public bool IsActive { get; set; }
-
+        [Required]
         public Cargo Cargo{ get; set; }
+        [Required]
         public Departamento Departamento { get; set; }
     }
 
     public class Departamento
     {
+        [Key]
         public int DepartamentoId { get; set; }
+        [Required]
         public int CodigoDepartamento { get; set; }
+        [Required]
+        public string Nombre { get; set; }
 
         public ICollection<Empleado> Empleados { get; set; }
     }
 
     public class Cargo
     {
+        [Key]
         public int CargoId { get; set; }
+        [Required]
         public string Nombre { get; set; }
 
         public ICollection<Empleado> Empleados { get; set; }
@@ -43,18 +53,24 @@ namespace DMHR.Models
 
     public class Nomina
     {
+        [Key]
         public int NominaId { get; set; }
+        [Required]
         public DateTime Year { get; set; }
+        [Required]
         public DateTime Month { get; set; }
         public int MontoTotal { get; set; }
     }
 
     public class SalidadeEmpleado
     {
+        [Key]
+        public int SalidaEmpleadoId { get; set; }
         public Salida TipoDeSalida { get; set; }
         public string Motivo { get; set; }
+        [Required]
         public DateTime FechaSalida { get; set; }
-
+        [Required]
         public Empleado Empleado { get; set; }
     }
 
@@ -67,31 +83,48 @@ namespace DMHR.Models
 
     public class Vacaciones
     {
+        [Key]
+        public int VacacionesId { get; set; }
+        [Required]
         public DateTime Desde { get; set; }
+        [Required]
         public DateTime Hasta { get; set; }
         public DateTime Date { get; set; }
         public string Comentarios { get; set; }
-
+        public bool IsPaid { get; set; }
+        [Required]
         public Empleado Empleado { get; set; }
     }
 
     public class Permisos
     {
+        [Key]
+        public int PermisosId { get; set; }
+        [Required]
         public DateTime Desde { get; set; }
+        [Required]
         public DateTime Hasta { get; set; }
         public DateTime Date { get; set; }
         public string Comentarios { get; set; }
+        public bool IsPaid { get; set; }
 
+        [Required]
         public Empleado Empleado { get; set; }
     }
 
     public class Licencias
     {
+        [Key]
+        public int LicenciasId { get; set; }
+        [Required]
         public DateTime Desde { get; set; }
+        [Required]
         public DateTime Hasta { get; set; }
         public string Motivo { get; set; }
         public string Comentarios { get; set; }
+        public bool IsPaid { get; set; }
 
+        [Required]
         public Empleado Empleado { get; set; }
     }
 }
